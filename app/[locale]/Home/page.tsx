5ae41c9190c54c09b65002b904c40cb29554e67f
Luciano <lucianosantillan96@outlook.com.ar>
2025-05-17 01:31:59 +0200

    feat: banner translations

--- app/[locale]/Home/page.tsx
+++ app/[locale]/Home/page.tsx
@@ -2,12 +2,20 @@ import Banner from "@/app/[locale]/Home/components/Banner/Banner";
 import Services from "@/app/[locale]/Home/components/Services/Services";
 import Articles from "@/app/components/Articles/Articles";
 import Contact from "@/app/[locale]/Home/components/Contact/Contact";
+import getRequestConfig from "@/src/i18n/request";
 
-export default function HomePage() {
+export default async function HomePage({ params }: { params: { locale: string } }) {
+  const { messages } = await getRequestConfig({ requestLocale: Promise.resolve(params.locale) });
 
   return (
     <>
-      <Banner />
+      <Banner
+        title={messages?.Banner?.title ?? ""}
+        name={messages?.Banner?.name ?? ""}
+        bullets={messages?.Banner?.bullets ?? []}
+        primaryCta={messages?.Banner?.primaryCta ?? ""}
+        secondaryCta={messages?.Banner?.secondaryCta ?? ""}
+      />
       <Services />
       <Articles />
       <Contact />
--- app/design_system/Text/Headings/Heading3/Heading3.tsx
+++ app/design_system/Text/Headings/Heading3/Heading3.tsx
@@ -2,7 +2,7 @@ import './Heading3.css';
 import { HeadingProps } from '../shared/HeadingProps';
 import '../shared/HeadingSizes.css';
 
-export default function Heading3({ children, className = "", size = "m", ...rest }: HeadingProps) {
+export default function Heading3({ children, className = "", size = "l", ...rest }: HeadingProps) {
   return (
     <h3 className={`heading3 heading-${size} ${className}`} {...rest}>
       {children}
